12a9689dfa6c3821c2e55f0aa75a1715e389acbb
SHAHRIAR <119577976+capslok12@users.noreply.github.com>
2025-12-19 11:21:12 +0600

    Update database.py

--- flood_backend/database.py
+++ flood_backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "postgresql://postgres:AllahMalik@localhost:5432/flood_support_db"
+DATABASE_URL = "postgresql://postgres:************@localhost:5432/flood_support_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
